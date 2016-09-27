57358948b3486c94250480ffb6121f238fc74851
AScripnic <scripnic.alexandru@gmail.com>
2016-09-27 18:42:22 +0300

    Add notification and more friendly UI for forms

--- src/deep-adtechmedia/frontend/js/contact.js
+++ src/deep-adtechmedia/frontend/js/contact.js
@@ -1,32 +1,101 @@
-function sendEmail(payload) {
+function handleCallback(error) {
+  if (error) {
+    return noty({
+      text: 'An error occurred during processing',
+      type: 'error',
+      timeout: 3000
+    });
+  }
+
+  return noty({
+    text: 'Thank you. We will contact you soon.',
+    type: 'success',
+    timeout: 3000
+  });
+}
+
+function disableForm(form) {
+  for (var index in form) {
+    if (!form.hasOwnProperty(index)) {
+      continue;
+    }
+
+    form[index].classList.add('disabled')
+  }
+}
+
+function enableForm(form) {
+  for (var index in form) {
+    if (!form.hasOwnProperty(index)) {
+      continue;
+    }
+
+    form[index].classList.remove('disabled')
+  }
+}
+
+function sendEmail(payload, callback) {
   var emailResource = DeepFramework.Kernel.get('resource').get('@deep-adtechmedia:email');
 
   DeepFramework.Kernel.get('security').anonymousLogin(function() {
     emailResource.request('send', payload).send(function(response) {
       if (response.isError) {
-        return console.log(response.error);
+        return callback(response.error);
       }
+
+      return callback(null, response.data);
     });
   });
 }
 
 function sendContactUsEmail() {
-  var payload = {
-    name: document.getElementById('name-field').value,
-    phone: document.getElementById('phone-field').value,
-    email: document.getElementById('email-field').value,
-    message: document.getElementById('message-field').value,
+  var formElement = new FormData(document.getElementById('contactus-form'));
+  var form = {
+    nameElement : document.getElementById('name-field'),
+    phoneElement : document.getElementById('phone-field'),
+    emailElement : document.getElementById('email-field'),
+    messageElement : document.getElementById('message-field'),
+    submitElement : document.getElementById('contact-button')
   };
 
-  sendEmail(payload);
+  var payload = {
+    name: form.nameElement.value,
+    phone: form.phoneElement.value,
+    email: form.emailElement.value,
+    message: form.messageElement.value,
+  };
+
+  disableForm(form);
+  sendEmail(payload, function(error) {
+    handleCallback(error);
+    enableForm(form);
+
+    if (!error) {
+      formElement.reset();
+    }
+  });
 
   return false;
 }
 
 function sendGetStartedEmail() {
-  var payload = {
-    email: document.getElementById('get-started-email').value
+  var emailElement = document.getElementById('get-started-email');
+
+  var form = {
+    email : emailElement
   };
 
-  sendEmail(payload);
+  var payload = {
+    email: emailElement.value
+  };
+
+  disableForm(form);
+  sendEmail(payload, function(error) {
+    handleCallback(error);
+    enableForm(form);
+
+    if (!error) {
+      emailElement.value = '';
+    }
+  });
 }
