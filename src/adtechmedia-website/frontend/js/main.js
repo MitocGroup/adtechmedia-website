42738725ffe9adf7c94d64bcde914fbd9e20b16b
icovali <icovali@pycoding.biz>
2017-04-07 15:22:52 +0300

    nav and flex issues on mobile

--- src/adtechmedia-website/frontend/js/main.js
+++ src/adtechmedia-website/frontend/js/main.js
@@ -2,8 +2,6 @@
 
 // @todo: split this file into related functionality (video, maps, etc)
 
-var videoBlock = document.getElementById('video-block');
-var videoFile = document.getElementById('video-file');
 var $menu = $('.main-nav ul');
 var headerHeight = $('header').height();
 var $menuIcon = $('#navTrigger');
@@ -30,35 +28,27 @@ function _initGoogleMaps() {
   }
 }
 
-function _videoShow() {
-  videoBlock.classList.remove('hidden');
-  videoFile.autoplay = true;
-  videoFile.load();
-}
-
-function _videoHide() {
-  videoBlock.classList.add('hidden');
-  videoFile.pause();
-  videoFile.currentTime = 0;
-}
-
 //show spinner while page loads
 $(window).load(function() {
   $(".loader").fadeOut("slow");
 });
 
 // Main nav on mobile
+function _clearNav(){
+  $menuIcon.removeAttr('checked');
+  $('body').removeClass('menu-shown');
+}
+
 $menuIcon.on('click', function(){
   if($menuIcon.prop('checked')) {
-    if($(window).height() > $menu.height() + headerHeight) {
-      $(window).on('scroll touchmove', function() {
-        $menuIcon.removeAttr('checked');
+    if($(window).height() > $menu.height() + headerHeight){
+      $(window).on('scroll', function() {
+        _clearNav();
       });
     } else {
       $('body').addClass('menu-shown');
       $menu.on('click', 'a', function() {
-        $menuIcon.removeAttr('checked');
-        $('body').removeClass('menu-shown');
+        _clearNav();
       });
     }
   } else {
@@ -66,8 +56,8 @@ $menuIcon.on('click', function(){
   }
 });
 
-$('.show-toggle').click(function() {
-  $(this).closest('.member-info').toggleClass('shown');
+$(window).on('orientationchange', function(){
+  _clearNav()
 });
 
 $(':required').on('blur keydown', function() {
@@ -84,3 +74,4 @@ new InputMask().Initialize(
 );
 
 _initGoogleMaps();
+
