6031df2c6fd44d294bb4268236744158119fa02d
icovali <icovali@pycoding.biz>
2017-05-12 16:54:23 +0300

    #117 add video

--- src/adtechmedia-website/frontend/js/main-page.js
+++ src/adtechmedia-website/frontend/js/main-page.js
@@ -25,3 +25,31 @@ $(window).on('scroll', function(){
     $('.scroll-down').removeClass('rotate');
   }
 });
+
+$('.video-show').on('click', function() {
+  var $video = $('.youtube-overlay');
+  var videoId = $video.data('video-id');
+
+  $video.removeClass('hidden');
+
+  // shows youtube video player
+  var player = new YT.Player($video.children('.youtube-video').attr('id'), {
+    height: '100%',
+    width: '100%',
+    videoId: videoId,
+    playerVars: {
+      autoplay: 1,
+      showinfo: 0,
+      rel: 0,
+      modestbranding: 1
+    },
+    events: {
+      'onStateChange': function(event) {
+        if (event.data === YT.PlayerState.ENDED) {
+          $('.youtube-overlay').addClass('hidden');
+          player.destroy();
+        }
+      }
+    }
+  });
+});
