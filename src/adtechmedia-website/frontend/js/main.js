7364b1d3707f734e67407f8a4ee023fd168604ee
ddimitrioglo <ddimitrioglo@pycoding.biz>
2017-05-18 12:49:48 +0300

    #ATM-WEB Chimp form redirect to 'thank-you-page'

--- src/adtechmedia-website/frontend/js/main.js
+++ src/adtechmedia-website/frontend/js/main.js
@@ -1,11 +1,9 @@
 /*eslint no-undef:0, no-unused-vars:0 */
 
-// @todo: split this file into related functionality (video, maps, etc)
-
 var $menu = $('.main-nav ul');
-var headerHeight = $('header').height();
 var $menuIcon = $('#navTrigger');
-
+var $demoForm = $('#mc-embedded-subscribe-form');
+var $demoFormSubmit = $('#mc-embedded-subscribe');
 
 //show spinner while page loads
 $(window).load(function() {
@@ -41,10 +39,24 @@ $('form').on('reset', function () {
   $(':required').removeClass('touched');
 });
 
-$('#mc-embedded-subscribe').on('click', function() {
-  $('#mc-embedded-subscribe-form').submit();
+/**
+ * Programmatically Chimp form submit
+ */
+$demoFormSubmit.on('click', function() {
+  $demoForm.submit();
 });
 
-if( navigator.userAgent.match(/iPhone|iPad|iPod/i) ) {
+/**
+ * Assign a callback to the Chimp form
+ */
+$demoForm.ajaxChimp({
+  callback: function (data) {
+    if (data.result === 'success') {
+      window.location = '/demo-confirmation'
+    }
+  }
+});
+
+if(navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
   $('body').addClass('mobile');
 }
