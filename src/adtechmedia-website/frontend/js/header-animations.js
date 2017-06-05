b3b338ff70a46d89543c6c6ed5524f3c8038e164
icovali <icovali@pycoding.biz>
2017-06-05 18:13:12 +0300

    code cleanup

--- src/adtechmedia-website/frontend/js/header-animations.js
+++ src/adtechmedia-website/frontend/js/header-animations.js
@@ -24,19 +24,16 @@
     wScrollDiff		= wScrollBefore - wScrollCurrent;
     elTop			= parseInt( window.getComputedStyle( element ).getPropertyValue( 'top' ) ) + wScrollDiff;
 
-    if( wScrollCurrent <= 0 ) // scrolled to the very top; element sticks to the top
-      element.style.top = '0px';
-
-    else if( wScrollDiff > 0 ) // scrolled up; element slides in
-      element.style.top = ( elTop > 0 ? 0 : elTop ) + 'px';
-
-    else if( wScrollDiff < 0 ) // scrolled down
-    {
-      if( wScrollCurrent + wHeight >= dHeight - elHeight )  // scrolled to the very bottom; element slides in
-        element.style.top = ( ( elTop = wScrollCurrent + wHeight - dHeight ) < 0 ? elTop : 0 ) + 'px';
-
-      else // scrolled down; element slides out
-        element.style.top = ( Math.abs( elTop ) > elHeight ? -elHeight : elTop ) + 'px';
+    if( wScrollCurrent <= 0 ) {
+      element.style.top = '0px'
+    } else if( wScrollDiff > 0 ) {
+      element.style.top = ( elTop > 0 ? 0 : elTop ) + 'px'
+    } else if( wScrollDiff < 0 ) {
+      if( wScrollCurrent + wHeight >= dHeight - elHeight ) {
+        element.style.top = ( ( elTop = wScrollCurrent + wHeight - dHeight ) < 0 ? elTop : 0 ) + 'px'
+      } else {
+        element.style.top = ( Math.abs( elTop ) > elHeight ? -elHeight : elTop ) + 'px'
+      }
     }
 
     wScrollBefore = wScrollCurrent;
