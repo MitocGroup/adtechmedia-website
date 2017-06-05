759de6d8b4fcde0e2a2b556b1d3e73ef35cde354
icovali <icovali@pycoding.biz>
2017-06-05 18:21:07 +0300

    code cleanup

--- src/adtechmedia-website/frontend/js/header-animations.js
+++ src/adtechmedia-website/frontend/js/header-animations.js
@@ -8,12 +8,12 @@
   if( !element ) return true;
 
   var elHeight = 0,
-    elTop	= 0,
-    dHeight	= 0,
-    wHeight	= 0,
+    elTop = 0,
+    dHeight = 0,
+    wHeight = 0,
     wScrollCurrent = 0,
-    wScrollBefore	= 0,
-    wScrollDiff	= 0;
+    wScrollBefore = 0,
+    wScrollDiff = 0;
 
   window.addEventListener( 'scroll', function()
   {
