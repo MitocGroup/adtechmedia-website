5beed8330c45ada18d575e75147d1f3e5960ff99
icovali <icovali@pycoding.biz>
2017-06-22 19:08:19 +0300

    #211 fix issues for safari

--- src/adtechmedia-website/frontend/js/medium-rss.js
+++ src/adtechmedia-website/frontend/js/medium-rss.js
@@ -7,9 +7,6 @@ $(function () {
     if (response.status == 'ok') {
       var output = '';
       $.each(response.items, function (k, item) {
-        var date = new Date(item.pubDate);
-        var options = { month: 'short', day: 'numeric' };
-        var trimmedDate = date.toLocaleDateString('en-US', options);
         var description = $(item.description);
         var trimmedString = description[0].innerText;
 
@@ -17,7 +14,7 @@ $(function () {
         output += '<h3 class="blog-title"><a href="'+ item.link + '" target="_blank">' + item.title + '</a></h3>';
         output += '<p class="post-description">' + trimmedString + '</p>';
         output += '<div class="post-meta"><span class="post-author"><a href="https://blog.adtechmedia.io/@MitocGroup" target="_blank">' + item.author + '</a></span>';
-        output += '<span class="post-date">' + trimmedDate + "</span></div>";
+        output += '<span class="post-date">' + $.format.date(item.pubDate, "MMM dd") + "</span></div>";
         output += '</div>';
         return k < 2;
       });
