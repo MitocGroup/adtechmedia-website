53c560683a184a7b6e629b8e769a404936528d90
avozicov <avozicov@mitocgroup.com>
2017-07-14 12:46:27 +0300

    Fix syntax issues and fix failed requestDemo script

--- test/e2e/test_scripts/form/requestDemo.e2e.js
+++ test/e2e/test_scripts/form/requestDemo.e2e.js
@@ -1,34 +1,34 @@
 import { Selector } from 'testcafe';
 
-fixture `"Request a Demo" form submit verification`
-  .page `https://www-dev.adtechmedia.io/`;
+fixture`"Request a Demo" form submit verification`
+  .page`https://www-test.adtechmedia.io/`;
 
-  test('Check if user can submit "Request a demo" form with valid email address', async t => {
-    const requestDemoTopMenuItem = await Selector('.custom-menu-link > a')
+test('Check if user can submit "Request a demo" form with valid email address', async t => {
+  const requestDemoTopMenuItem = await Selector('.custom-menu-link > a');
 
-    await t
-      .resizeWindow(1920, 1080)
-      .click(requestDemoTopMenuItem, { speed: 0.5 })
-      .expect(Selector('#modal').with({
-        selectorTimeout: 5000,
-        visibilityCheck: true,
-      }).visible).ok();
+  await t
+    .resizeWindow(1920, 1080)
+    .click(requestDemoTopMenuItem, { speed: 0.5 })
+    .expect(Selector('#modal').with({
+      selectorTimeout: 5000,
+      visibilityCheck: true,
+    }).visible).ok()
 
-      const demoFormEmailField = await Selector('#mce-EMAIL')
-      const demoFormNamelField = await Selector('#mce-FNAME')
-      const demoFormSubmitButton = await Selector('#mc-embedded-subscribe');
+  const demoFormEmailField = await Selector('#mce-EMAIL')
+  const demoFormNamelField = await Selector('#mce-FNAME')
+  const demoFormSubmitButton = await Selector('#mc-embedded-subscribe');
 
-    await t
-      .typeText(demoFormEmailField, 'sakusha@yahoo.ca')
-      .typeText(demoFormNamelField, 'Test User')
-      .click(demoFormSubmitButton, { speed: 0.5 });
+  await t
+    .typeText(demoFormEmailField, 'sakusha@yahoo.ca')
+    .typeText(demoFormNamelField, 'Test User')
+    .click(demoFormSubmitButton, { speed: 0.5 })
 
-      const demoFormResponseText = await Selector('.demo-confirm');
+  const demoFormResponseText = await Selector('.demo-confirm');
 
-    await t
-      .expect(Selector(demoFormResponseText).with({
-        selectorTimeout: 5000,
-        visibilityCheck: true,
-      }).visible).ok()
-      .expect(Selector(demoFormResponseText).innerText).eql('\nThank you for the interest in AdTechMedia WordPress Plugin\nPlease check your inbox to schedule your demo.\n');
-  });
+  await t
+    .expect(Selector(demoFormResponseText).with({
+      selectorTimeout: 5000,
+      visibilityCheck: true,
+    }).visible).ok()
+    .expect(Selector(demoFormResponseText).innerText).eql('\nThank you for the interest in AdTechMedia WordPress Plugin\nPlease check your inbox to schedule your demo.\n');
+});
