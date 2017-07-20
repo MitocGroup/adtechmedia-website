3c6002347037e48e62fe843c4d9ca99d181d68f6
ddimitrioglo <ddimitrioglo@pycoding.biz>
2017-07-20 10:54:10 +0300

    #ATM-WEB restructuring + website as standalone microapp

---
+++ src/adtechmedia-website/frontend/js/src/case-studies.js
@@ -0,0 +1,19 @@
+/* global DeepFramework */
+
+var url = 'manage.adtechmedia.io';
+
+DeepFramework.Kernel.bootstrap(function() {
+  var dashboardUrl = DeepFramework.Kernel.config.microservices['adtechmedia-website'].parameters.dashboardUrl;
+
+  if (dashboardUrl) {
+    url = dashboardUrl;
+  }
+});
+
+function onRegister() {
+  window.location = url + '/accounts/signup'
+}
+
+function onLogin() {
+  window.location = url + '/accounts/signin'
+}
