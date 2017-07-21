f3c368ed0ee3d2f7099612b63fc4d7492d3fa247
avozicov <avozicov@mitocgroup.com>
2017-07-21 17:38:13 +0300

    Wait for e2e test url read from env vars

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -1,4 +1,4 @@
 export default {
-  www_base_host: process.env.WWW_BASE_HOST || 'https://www-stage.adtechmedia.io'
+  www_base_host: process.env.WWW_BASE_HOST || 'https://www-test.adtechmedia.io'
 }
 
