65b3953eda4b01cffd537e9bfd6c51e3849d723d
avozicov <avozicov@pycoding.biz>
2017-08-17 15:08:38 +0300

    Debuging Travis execution

--- test/e2e/tests/v1/assert-header-component.e2e.js
+++ test/e2e/tests/v1/assert-header-component.e2e.js
@@ -15,59 +15,62 @@ fixture`Check valid content is displayed on website header`
 
 test('Check "Logo" image is displayed on website header', async t => {
   await t
-    .expect(sharedFunctions.visible(header.logoImage)).ok()
+    .expect(header.logoImage.with({
+      selectorTimeout: 5000,
+      visibilityCheck: true,
+    }).visible).ok()
 });
 
-test('Check "CHALLENGES" top-menu link is displayed on website header', async t => {
-  await t
-    .expect(sharedFunctions.visible(header.challengesTopMenuLink)).ok()
-    .expect(header.challengesTopMenuLink.innerText).match(
-      sharedFunctions.anyCase('CHALLENGES')
-    );
-});
+// test('Check "CHALLENGES" top-menu link is displayed on website header', async t => {
+//   await t
+//     .expect(sharedFunctions.visible(header.challengesTopMenuLink)).ok()
+//     .expect(header.challengesTopMenuLink.innerText).match(
+//       sharedFunctions.anyCase('CHALLENGES')
+//     );
+// });
 
-test('Check "SOLUTIONS" top-menu link is displayed on website header', async t => {
-  await t
-    .expect(sharedFunctions.visible(header.solutionsTopMenuLink)).ok()
-    .expect(header.solutionsTopMenuLink.innerText).match(
-      sharedFunctions.anyCase('SOLUTIONS')
-    );
-});
+// test('Check "SOLUTIONS" top-menu link is displayed on website header', async t => {
+//   await t
+//     .expect(sharedFunctions.visible(header.solutionsTopMenuLink)).ok()
+//     .expect(header.solutionsTopMenuLink.innerText).match(
+//       sharedFunctions.anyCase('SOLUTIONS')
+//     );
+// });
 
-test('Check "API" top-menu link is displayed on website header', async t => {
-  await t
-    .expect(sharedFunctions.visible(header.apiTopMenuLink)).ok()
-    .expect(header.apiTopMenuLink.innerText).match(
-      sharedFunctions.anyCase('API')
-    );
-});
+// test('Check "API" top-menu link is displayed on website header', async t => {
+//   await t
+//     .expect(sharedFunctions.visible(header.apiTopMenuLink)).ok()
+//     .expect(header.apiTopMenuLink.innerText).match(
+//       sharedFunctions.anyCase('API')
+//     );
+// });
 
-test('Check "TEAM" top-menu link is displayed on website header', async t => {
-  await t
-    .expect(sharedFunctions.visible(header.teamTopMenuLink)).ok()
-    .expect(header.teamTopMenuLink.innerText).match(
-      sharedFunctions.anyCase('TEAM')
-    );
-});
+// test('Check "TEAM" top-menu link is displayed on website header', async t => {
+//   await t
+//     .expect(sharedFunctions.visible(header.teamTopMenuLink)).ok()
+//     .expect(header.teamTopMenuLink.innerText).match(
+//       sharedFunctions.anyCase('TEAM')
+//     );
+// });
 
-test('Check "CONTACT" top-menu link is displayed on website header', async t => {
-  await t
-    .expect(sharedFunctions.visible(header.contactTopMenuLink)).ok()
-    .expect(header.contactTopMenuLink.innerText).match(
-      sharedFunctions.anyCase('CONTACT')
-    );
-});
+// test('Check "CONTACT" top-menu link is displayed on website header', async t => {
+//   await t
+//     .expect(sharedFunctions.visible(header.contactTopMenuLink)).ok()
+//     .expect(header.contactTopMenuLink.innerText).match(
+//       sharedFunctions.anyCase('CONTACT')
+//     );
+// });
 
-test('Check "BLOG" top-menu link is displayed on website header', async t => {
-  await t
-    .expect(sharedFunctions.visible(header.blogTopMenuLink)).ok()
-    .expect(header.blogTopMenuLink.innerText).match(
-      sharedFunctions.anyCase('BLOG')
-    );
-});
+// test('Check "BLOG" top-menu link is displayed on website header', async t => {
+//   await t
+//     .expect(sharedFunctions.visible(header.blogTopMenuLink)).ok()
+//     .expect(header.blogTopMenuLink.innerText).match(
+//       sharedFunctions.anyCase('BLOG')
+//     );
+// });
 
-test('Check "Request a Demo" modal is displayed on website header', async t => {
-  await t 
-    .expect(sharedFunctions.visible(header.topMenuRequestDemoModal)).ok()
-    .click(header.topMenuRequestDemoModal);
-});
+// test('Check "Request a Demo" modal is displayed on website header', async t => {
+//   await t 
+//     .expect(sharedFunctions.visible(header.topMenuRequestDemoModal)).ok()
+//     .click(header.topMenuRequestDemoModal);
+// });
