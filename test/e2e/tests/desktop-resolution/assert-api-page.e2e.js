a5771878e589b801c75c5dcfb029c2bab30d1d23
avozicov <avozicov@pycoding.biz>
2017-08-24 20:21:41 +0300

    Debug - enabling multibrowsers support

--- test/e2e/tests/desktop-resolution/assert-api-page.e2e.js
+++ test/e2e/tests/desktop-resolution/assert-api-page.e2e.js
@@ -8,27 +8,22 @@ const api = new Api();
 const fix = fixture`Check valid content is displayed on "API" page`
   .page`${config.www_base_host}/api`;
 
-// sharedFunctions.fictureResize(fix);
+sharedFunctions.fictureResize(fix);
    
 test('Assert "Case-Studies" content is displayed on "API" page', async t => {
   await t
-    .maximizeWindow()  
     .expect(sharedFunctions.visible(api.caseStudies)).ok()
     .expect(api.caseStudies.innerText).contains('Want to learn about what you can achieve by integrating with our APIs? The possibilities are endless, but you can find just a few examples');
 });
 
 test('Assert "API-Docs" content is displayed on "API" page', async t => {
-  await t
-    .resizeWindow(1200, 600)  
+  await t  
     .expect(sharedFunctions.visible(api.elementApiDocs)).ok()
     .expect(api.elementApiDocs.innerText).contains('See what APIs we have on offer, including extensive documentation. Sign in to manage your subscriptions, see your current usage, get your API Key, and test against our live API.');
 });
 
 test('Assert "User-Guide" content is displayed on "API" page', async t => {
   await t
-    .resizeWindowToFitDevice('iPhone 6 Plus', {
-      portraitOrientation: false
-  })
     .expect(sharedFunctions.visible(api.elementUserGuide)).ok()
     .expect(api.elementUserGuide.innerText).contains('Ready to get started? This is the place that answers all your questions. We\'ll have you up and running in no time. Let\'s get started!');
 });
