a63813c0585312388ff7cd14f86c4c2c493420d4
Eugene <eistrati@mitocgroup.com>
2017-12-18 09:37:19 -0500

    back and restore provisioning file

--- bin/deploy/jenkins.sh
+++ bin/deploy/jenkins.sh
@@ -46,7 +46,7 @@ aws s3 cp s3://deep-deploy-assets/atm-website/test/ src/ --recursive --exclude "
 echo "Deploying website"
 export DEEP_NO_INTERACTION=1
 export DEEP_CONFIRMATION_REFUSE=1
-
 deepify deploy src --loglevel=debug
 
+echo "Backup provisioning file"
 aws s3 cp src/ s3://deep-deploy-assets/atm-website/test/ --recursive --exclude "*" --include "*.provisioning.json"
