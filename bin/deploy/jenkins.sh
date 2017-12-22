9621920a629df4e0985ec4b4828d62c686b34011
Dmitri Dimitrioglo <ddimitrioglo@pycoding.biz>
2017-12-22 03:39:49 +0200

    #ATM-WEB move to jenkins (#377)
    * #ATM-WEB move to jenkins
    
    * #ATM-WEB add message
    
    * #ATM-WEB update message
    
    * #ATM-WEB update deps
    
    * #ATM-WEB downgrade deepify version
    
    * #ATM-WEB use node v6
    
    * #ATM-WEB use node v6 (update)
    
    * #ATM-WEB configure deeploy.json
    
    * #ATM-WEB switch to adtechmedia aws account
    
    * #ATM-WEB update bucket name
    
    * #ATM-WEB update bucket name v2
    
    * #ATM-WEB update bucket name v3
    
    * #ATM-WEB update bucket name v4
    
    * copy provisioning file
    
    * backup and restore files
    
    * improve s3 backup and restore
    
    * remove params
    
    * back and restore provisioning file
    
    * backup and restore provisioning file

---
+++ bin/deploy/jenkins.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+configure_npm() {
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" > /dev/null
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" > /dev/null
+
+    nvm install 6
+}
+
+ensure_required_deps() {
+    pip install --user awscli > /dev/null
+
+    NPM_BIN=`which npm`
+    REQUIRED_DEPS=(
+        deepify
+        recink
+        recink-snyk
+        recink-pagespeed
+        recink-codeclimate
+        aws-sdk
+        uglifyjs-webpack-plugin
+        mishoo/UglifyJS2#harmony-v2.8.22
+        webpack
+    );
+
+    for DEP in ${REQUIRED_DEPS[@]}; do
+        if [ ! -d "$(${NPM_BIN} root -g)/${DEP}" ]; then
+            echo "Installing missing ${DEP}"
+            ${NPM_BIN} install -g ${DEP} || (echo "Failed to install ${DEP}" && exit 1)
+        fi
+    done
+}
+
+echo "Configuring NPM"
+configure_npm
+
+echo "Installing required dependencies"
+ensure_required_deps
+
+echo "Updating parameters"
+aws s3 cp s3://deep-deploy-assets/atm-website/test/.parameters.json src/adtechmedia-website/
+aws s3 cp s3://deep-deploy-assets/atm-website/test/deeploy.json src/
+aws s3 cp s3://deep-deploy-assets/atm-website/test/ src/ --recursive --exclude "*" --include "*.provisioning.json" || echo "Provisioning file is missing in S3"
+
+echo "Deploying website"
+export DEEP_NO_INTERACTION=1
+export DEEP_CONFIRMATION_REFUSE=1
+
+deepify deploy src --loglevel=debug
+
+echo "Backup provisioning file"
+aws s3 cp src/ s3://deep-deploy-assets/atm-website/test/ --recursive --exclude "*" --include "*.provisioning.json"
